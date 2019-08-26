4ead04f20dc9b460a04293a34b1e5b3fe5645871
Alexandr <alex3289@gmail.com>
2019-08-26 16:02:24 +0600

    Добавлен генератор сылок

---
+++ WebApplication1/Models/LinkGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    /// <summary>
+    /// Класс для генерации ссылок
+    /// </summary>
+    public static class LinkGenerator
+    {
+        /// <summary>
+        /// Герератор коротких ссылок
+        /// </summary>
+        /// <returns>Короткая ссылка</returns>
+        public static string GeneratorShort()
+        {
+            string resul = string.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                resul += RandomCharacterGenerator().ToString();
+            }
+
+            return string.Format("/{0}/", resul);
+        }
+
+        /// <summary>
+        /// Генерирует случайный чисел
+        /// </summary>
+        /// <returns>Случайное число</returns>
+        private static int RandomCharacterGenerator()
+        {
+            Random rand = new Random();
+            return rand.Next(0, 9);
+        }
+    }
+}
